5cb59c20c471e0f8d0b6a36f94c3661480d2ddd7
Patrick Kettner <patrickkettner@gmail.com>
2018-11-05 14:56:16 -0500

    .

--- .netlify/functions/download.js
+++ .netlify/functions/download.js
@@ -1,6 +1,17 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: event.headers['user-agent']
+    body: `
+      <!doctype html>
+      <html>
+        <head>
+          <title>testing 123</title>
+          <script>history.replaceState({}, "yes!", "/download")</script>
+        </head>
+        <body>
+          <pre>${event.headers['user-agent']}</pre>
+        </body>
+      </html>
+    `
   });
 }

4a909d9071a9cb18074ea96e7acb99bbf7dd4d67
Patrick Kettner <patrickkettner@gmail.com>
2020-01-02 19:09:27 -0500

    .

--- .netlify/functions/download.js
+++ .netlify/functions/download.js
@@ -3,38 +3,38 @@ exports.handler = function(event, context, callback) {
   let responseObj = {}
 
   if (UA.includes('Chrome')) {
-  responseObj = {
-    statusCode: 200,
-    body: ` <!doctype html>
-      <html>
-        <head>
-          <title>testing 123</title>
-          <script>history.replaceState({}, "yes!", "/download")</script>
-        </head>
-        <body>
-          <pre>${event.headers['user-agent']}</pre>
-        </body>
+    responseObj = {
+      "statusCode": 200,
+      "body": ` <!doctype html>
+        <html>
+          <head>
+            <title>testing 123</title>
+            <script>history.replaceState({}, "yes!", "/download")</script>
+          </head>
+          <body>
+            <pre>${event.headers['user-agent']}</pre>
+          </body>
       </html>`
-  }
+    }
   } else if (!!UA.match(/^npm\//)) {
-	  responseObj = {
-		  statusCode: 302,
-		  location: '/test.tar.gz'
-	  }
+    responseObj = {
+      "statusCode": 302,
+      "location": '/test.tar.gz'
+    }
   } else {
-  responseObj = {
-    statusCode: 200,
-  body = `<!doctype html>
-      <html>
-        <head>
-          <title>testing 123</title>
-        </head>
-        <body>
-<pre>this is not chrome</chrome>
-        </body>
-      </html>`
-}
-}
+    responseObj = {
+      'statusCode": 200,
+      "body": `<!doctype html>
+        <html>
+          <head>
+            <title>testing 123</title>
+          </head>
+          <body>
+          <pre>this is not chrome</chrome>
+          </body>
+        </html>`
+    }
+  }
 
   callback(null, responseObj);
 }
